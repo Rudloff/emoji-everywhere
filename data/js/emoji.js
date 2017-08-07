9e9742c19ffd6994bd605e3bbf2fb365b578f95f
schrikvis <schrikvis@gmail.com>
2017-08-07 16:31:42 +0200

    Convert to webextension

--- data/js/emoji.js
+++ data/js/emoji.js
@@ -1,8 +1,1 @@
-/*global twemoji, self*/
-/*jslint browser: true, devel: true*/
-if (twemoji === undefined) {
-    console.error("Can't load Twemoji");
-} else {
-    twemoji.base = self.options.twemojiUrl;
-    twemoji.parse(document.body);
-}
+twemoji.parse(document.body);
